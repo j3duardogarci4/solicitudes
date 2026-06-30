28184921fa9580dc3d84a2eff15cdd1f749cca31
Juan E. GM <juanedo77@gmail.com>
2026-06-29 23:31:03 -0400

    Update ComentarioService.java

--- src/main/java/cl/j3duardogarci4/solicitudes/domain/comentario/ComentarioService.java
+++ src/main/java/cl/j3duardogarci4/solicitudes/domain/comentario/ComentarioService.java
@@ -3,7 +3,7 @@ package cl.j3duardogarci4.solicitudes.domain.comentario;
    public class ComentarioService {
     private final ComentarioRepository comentarioRepository;
     private final AuditoriaService auditoriaService;
-    public void ComentarioService(ComentarioRepository comentarioRepository, AuditoriaService auditoriaService){
+    public ComentarioService(ComentarioRepository comentarioRepository, AuditoriaService auditoriaService){
        this.auditoriaService = auditoriaService;
        this.comentarioRepository = comentarioRepository;
     }   

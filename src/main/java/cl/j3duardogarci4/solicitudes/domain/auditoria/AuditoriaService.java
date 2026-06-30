12304afcf60b0bb6c364133426e4e564eaefa6eb
Juan E. GM <juanedo77@gmail.com>
2026-06-29 23:28:13 -0400

    Update AuditoriaService.java

--- src/main/java/cl/j3duardogarci4/solicitudes/domain/auditoria/AuditoriaService.java
+++ src/main/java/cl/j3duardogarci4/solicitudes/domain/auditoria/AuditoriaService.java
@@ -3,6 +3,10 @@ package cl.j3duardogarci4.solicitudes.domain.auditoria;
 import cl.j3duardogarci4.solicitudes.domain.usuario.Usuario;
 
 public class AuditoriaService {
+    private final AuditoriaRepository auditoriaRepository;
+    public AuditoriaService(AuditoriaRepository auditoriaRepository){
+        this.auditoriaRepository = auditoriaRepository;
+    }
 
     public void registrarAccion(Long idSolicitud, String accion, Usuario usuario) {
 

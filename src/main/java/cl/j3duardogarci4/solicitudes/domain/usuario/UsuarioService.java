b48671024849c62325dd8fb59887b90514f66e12
Juan E. GM <juanedo77@gmail.com>
2026-06-29 23:12:11 -0400

    Update UsuarioService.java

--- src/main/java/cl/j3duardogarci4/solicitudes/domain/usuario/UsuarioService.java
+++ src/main/java/cl/j3duardogarci4/solicitudes/domain/usuario/UsuarioService.java
@@ -1,7 +1,13 @@
 package cl.j3duardogarci4.solicitudes.domain.usuario;
 
 public class UsuarioService {
-
+    private final UsuarioRepository usuarioRepository;
+    private final AuditoriaService auditoriaService;
+    
+    public void UsuarioService(UsuarioRepository usuarioRepository, AuditoriaService auditoriaService{
+        this.usuarioRepository = usuarioRepository;
+        this.auditoriaService = auditoriaService;
+    };
     public void crearUsuario(Usuario usuario) {
 
     }

23ddf840925ebe6b0e4dc2834e2219594632a9a1
Juan E. GM <juanedo77@gmail.com>
2026-06-30 00:05:51 -0400

    Update WorkflowService.java

--- src/main/java/cl/j3duardogarci4/solicitudes/domain/workflow/WorkflowService.java
+++ src/main/java/cl/j3duardogarci4/solicitudes/domain/workflow/WorkflowService.java
@@ -5,7 +5,20 @@ import cl.j3duardogarci4.solicitudes.domain.solicitud.Solicitud;
 import cl.j3duardogarci4.solicitudes.domain.usuario.Usuario;
 
 public class WorkflowService {
-
+    private final UsuarioRepository usuarioRepository;
+    private final SolicitudRepository solicitudRepository;
+    private final ComentarioRepository comentarioRepository;
+    
+    public WorkflowService(
+            SolicitudRepository solicitudRepository, 
+            UsuarioRepository usuarioRepository, 
+            ComentarioRepository comentarioRepository){
+        
+        this.usuarioRepository = usuarioRepository;
+        this.solicitudRepository = solicitudRepository;
+        this.comentarioRepository = comentarioRepository;
+        
+    }
     public void iniciarRevision(Solicitud solicitud, Usuario usuario) {
 
     }

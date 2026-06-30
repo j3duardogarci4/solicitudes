a76e50cbb979c6e94c80fab104483c02b7a9ce5d
Juan E. GM <juanedo77@gmail.com>
2026-06-29 23:35:28 -0400

    Update ComentarioService.java

--- src/main/java/cl/j3duardogarci4/solicitudes/domain/comentario/ComentarioService.java
+++ src/main/java/cl/j3duardogarci4/solicitudes/domain/comentario/ComentarioService.java
@@ -1,18 +1,26 @@
 package cl.j3duardogarci4.solicitudes.domain.comentario;
 
+import java.util.List;
+import cl.j3duardogarci4.solicitudes.domain.auditoria.AuditoriaService;
+
    public class ComentarioService {
+      
     private final ComentarioRepository comentarioRepository;
     private final AuditoriaService auditoriaService;
-    public ComentarioService(ComentarioRepository comentarioRepository, AuditoriaService auditoriaService){
-       this.auditoriaService = auditoriaService;
-       this.comentarioRepository = comentarioRepository;
+    
+      public ComentarioService(
+         ComentarioRepository comentarioRepository, 
+         AuditoriaService auditoriaService) {
+       
+         this.auditoriaService = auditoriaService;
+         this.comentarioRepository = comentarioRepository;
     }   
     public void registrarComentario(Comentario comentario) {
 
     }
 
-    public void obtenerComentarios(Long idSolicitud) {
-
+    public  List<Comentario> obtenerComentarios(Long idSolicitud) {
+        return List.of();
     }
 
 }
